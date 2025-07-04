0a29c43cbd7fd1057d0673739ce13123a5046ad0
Steven Matos <steven_matos@ymail.com>
2025-07-04 02:29:53 -0400

    update folder structure

--- EmailCleanUITests/EmailCleanUITestsLaunchTests.swift
+++ EmailCleanUITests/EmailCleanUITestsLaunchTests.swift
@@ -1,3 +1,10 @@
+//
+//  EmailCleanUITestsLaunchTests.swift
+//  EmailCleanUITests
+//
+//  Created by Steven Matos on 7/4/25.
+//
+
 import XCTest
 
 final class EmailCleanUITestsLaunchTests: XCTestCase {
@@ -10,16 +17,17 @@ final class EmailCleanUITestsLaunchTests: XCTestCase {
         continueAfterFailure = false
     }
 
+    @MainActor
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Insert steps here to perform after app launch but before taking a screenshot
-        // In the case of a description, it would be before the description is shown
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
 
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
         attachment.lifetime = .keepAlways
         add(attachment)
     }
-} 
+}
